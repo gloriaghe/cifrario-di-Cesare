e3077409a1e77f3d89aaa58afc2b8b03c3718865
gloriaghe <gloghe@yahoo.it>
2022-10-30 10:21:29 +0100

    cifratura funzionante

--- Program.cs
+++ Program.cs
@@ -1,1 +1,44 @@
-﻿
+﻿//Realizzare un programma in grado di CRIPTARE e DECRIPTARE una stringa inserita nell’utente con la strategia di criptazione nota come “IL CIFRARIO DI CESARE”
+//Mi raccomando ci sono diversi modi di integrarlo, scegliete una strategia semplice in base a quello che abbiamo spiegato:
+//l’utente inserisce una stringa da criptare / decriptare
+//l’utente inserisce una chiave numerica per effettuare la criptazione / decriptazione della stringa inserita
+Console.WriteLine("Inserisci la frase o parola da cifrare");
+string userString = (Console.ReadLine());
+userString = userString.ToUpper();
+
+Console.WriteLine("Inserisci la chiave numerica da utilizzare");
+int numericKey = Convert.ToInt32(Console.ReadLine());
+string cifrato = "";
+
+for (int i = 0; i < userString.Length; i++)
+{
+    int letter = (Convert.ToInt32(userString[i]));
+    int letterModify = 0;
+
+    if (letter == 32)
+        letterModify = letter;
+    else if (letter >= 48 && letter <= 57)
+    {
+
+        letterModify = letter + numericKey;
+        if (letterModify >= 57)
+        {
+            int number = letterModify - 57;
+            letterModify = 47 + number;
+        }
+    }
+    else if (letter >= 65 && letter <= 90)
+    {
+        letterModify = letter + numericKey;
+        if (letterModify >= 90)
+        {
+            int number = letterModify - 90;
+            letterModify = 64 + number;
+        }
+    }
+
+    char letterConvert = Convert.ToChar(letterModify);
+    Console.Write(letterConvert);
+}
+
+
